751153116f930b6b42e2e8c777d1dd81713ce8bd
kennetm <kenneth.macleod@sbs.com.au>
2024-05-21 13:14:54 +1000

    update es lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,44 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
     allowImportExportEverywhere: false,
     ecmaFeatures: {
       globalReturn: false,
     },
     babelOptions: {
-      configFile: "./babel.config.json",
+      configFile: './babel.config.json',
     },
   },
-  plugins: ["prettier", "styled-components-a11y"],
+  plugins: ['prettier', 'styled-components-a11y'],
   extends: [
-    "airbnb",
-    "plugin:prettier/recommended",
-    "plugin:styled-components-a11y/recommended",
-    "prettier", // always make sure prettier is last
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:styled-components-a11y/recommended',
+    'prettier', // always make sure prettier is last
   ],
   rules: {
-    "import/prefer-default-export": "off",
-    "prettier/prettier": ["error"],
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "no-nested-ternary": "off",
-    "react/jsx-props-no-spreading": "off",
-    radix: "off",
-    "no-console": "off",
-    "no-restricted-exports": "off",
-    "default-param-last": "off",
-    "react/prop-types": "off",
-    "import/no-dynamic-require": "off",
-    "import/no-extraneous-dependencies": [
-      "error",
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': ['error'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-nested-ternary': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    radix: 'off',
+    indent: 'off',
+    'no-console': 'off',
+    'no-restricted-exports': 'off',
+    'default-param-last': 'off',
+    'react/prop-types': 'off',
+    'import/no-dynamic-require': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
       {
         devDependencies: true,
         optionalDependencies: false,
         peerDependencies: false,
       },
     ],
-    "prefer-destructuring": ["error", { object: false, array: false }],
+    'prefer-destructuring': ['error', { object: false, array: false }],
   },
   env: {
     browser: true,
