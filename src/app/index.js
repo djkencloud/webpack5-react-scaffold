9938266309f06249a2862266361f3cb17d9702f7
kennetm <kenneth.macleod@sbs.com.au>
2025-03-11 15:08:22 +1100

    updates

--- src/app/index.js
+++ src/app/index.js
@@ -5,12 +5,8 @@ import { Provider } from 'react-redux';
 import GlobalStyles from './global-style';
 
 import { fetchData } from './state/slices/data';
-import slices from './state/slices/browserUtils';
 
-import Header from './views/header';
-import Content from './views/content';
-import Footer from './views/footer';
-
+import PageWrap from './views/pagewrap';
 import store from './state/store';
 
 const container = document.getElementById('foot');
@@ -19,31 +15,11 @@ const root = createRoot(container);
 const element = (
   <Provider store={store}>
     <GlobalStyles />
-    <div className="page-wrap">
-      <Header />
-      <Content />
-      <Footer />
-    </div>
+    <PageWrap />
   </Provider>
 );
 
 root.render(element);
-
-/* Start up - listen for changes */
-const { onResize, onDarkMode, onOrientationChange } = slices.actions;
-
-const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-mediaQuery.addEventListener('change', () => store.dispatch(onDarkMode()));
-
-window.addEventListener('resize', () => {
-  store.dispatch(onResize());
-  store.dispatch(onOrientationChange());
-});
-
-window.addEventListener('orientationchange', () => {
-  store.dispatch(onResize());
-  store.dispatch(onOrientationChange());
-});
 
 /* Initial data fetch */
 store.dispatch(fetchData());
---
+++ src/app/views/pagewrap/pagewrapstyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const PageWrapStyles = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 1400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
